378aba44d7299db0d675f482b6cdad80e3092824
dhanisalghifari <dhanisalghifari@gmail.com>
2021-06-06 15:55:16 +0700

    use axios for redux api call

--- src/containers/ProductListing.js
+++ src/containers/ProductListing.js
@@ -1,16 +1,27 @@
-import React from 'react'
-
-import ProductComponent from './ProductComponent';
+import React from "react";
+import axios from "axios";
+import ProductComponent from "./ProductComponent";
+import { useEffect } from "react";
 
 function ProductListing() {
+  const fetchProducts = async () => {
+    const response = await axios
+      .get("https://fakestoreapi.com/products")
+      .catch((err) => {
+        console.log("error", err);
+      });
+    console.log(response.data);
+  };
 
-  
+  useEffect(() => {
+    fetchProducts();
+  }, []);
 
-    return (
-        <div className="ui grid container" style={{marginTop: '4rem'}}>
-            <ProductComponent/>
-        </div>
-    )
+  return (
+    <div className="ui grid container" style={{ marginTop: "4rem" }}>
+      <ProductComponent />
+    </div>
+  );
 }
 
-export default ProductListing
+export default ProductListing;
