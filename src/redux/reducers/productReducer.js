9b4fd7d07d8db747a23509e7c649706d8f55e672
dhanisalghifari <dhanisalghifari@gmail.com>
2021-06-06 15:03:12 +0700

    connect react with redux

--- src/redux/reducers/productReducer.js
+++ src/redux/reducers/productReducer.js
@@ -8,13 +8,13 @@ const initialState = {
     }]
 }
 
-export const productReducer = (state, {type, payload}) => {
+export const productReducer = (state = initialState, {type, payload}) => {
 
     switch (type) {
         case ActionTypes.SET_PRODUCTS:
             return state;
         default:
-            state;
+          return state;
     }
 
-}
+};
