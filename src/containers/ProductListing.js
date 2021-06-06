425c86918054a0330368d344012d17706fc9ba66
dhanisalghifari <dhanisalghifari@gmail.com>
2021-06-06 20:47:53 +0700

    refactore code to using redux-thunk

--- src/containers/ProductListing.js
+++ src/containers/ProductListing.js
@@ -1,27 +1,16 @@
 import React from "react";
-import axios from "axios";
 import ProductComponent from "./ProductComponent";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { setProducts, fetchProducts } from "../redux/actions/productActions";
+import { fetchProducts } from "../redux/actions/productActions";
 
 function ProductListing() {
   const dispatch = useDispatch();
   const products = useSelector((state) => state.allProducts.products);
-
-//   const fetchProducts = async () => {
-//     const response = await axios
-//       .get("https://fakestoreapi.com/products")
-//       .catch((err) => {
-//         console.log("error", err);
-//       });
-//     dispatch(setProducts(response.data));
-//   };
-
   useEffect(() => {
     dispatch(fetchProducts());
   }, []);
-  console.log('products: ', products);
+  console.log("products: ", products);
 
   return (
     <div className="ui grid container">
--- src/redux/actions/productActions.js
+++ src/redux/actions/productActions.js
@@ -1,31 +1,32 @@
-import axios from "axios"
-import { ActionTypes } from "../constants/action-types"
+import fakeStoreApi from "../../api/fakeStoreApi";
+import { ActionTypes } from "../constants/action-types";
 
-export const fetchProducts = async () => {
-    const response = await axios.get("/products");
-    console.log('ini respon di actions', response);
-    return {
-        type: ActionTypes.SET_PRODUCTS,
-        payload: response, 
-    }
-}
+export const fetchProducts = () => async (dispatch) => {
+  const response = await fakeStoreApi.get("/products");
+  dispatch({ type: ActionTypes.FETCH_PRODUCT, payload: response.data });
+};
+
+export const fetchProduct = (id) => async (dispatch) => {
+  const response = await fakeStoreApi.get(`/products/${id}`);
+  dispatch({ type: ActionTypes.SELECTED_PRODUCT, payload: response.data });
+};
 
 export const setProducts = (products) => {
-    return {
-        type: ActionTypes.SET_PRODUCTS,
-        payload: products, 
-    }
-}
+  return {
+    type: ActionTypes.SET_PRODUCTS,
+    payload: products,
+  };
+};
 
 export const selectedProduct = (product) => {
-    return {
-        type: ActionTypes.SELECTED_PRODUCT,
-        payload: product, 
-    }
-}
+  return {
+    type: ActionTypes.SELECTED_PRODUCT,
+    payload: product,
+  };
+};
 
 export const removeSelectedProduct = () => {
-    return {
-        type: ActionTypes.REMOVE_SELECTED_PRODUCT,
-    }
-}
+  return {
+    type: ActionTypes.REMOVE_SELECTED_PRODUCT,
+  };
+};
